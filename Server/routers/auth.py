1d22ab3c859ec34081a49d0550f1e1f45b9e36ef
AliuQudus <qudusaliu98@gmail.com>
2025-09-08 05:33:05 +0100

    Task: Formatted the code to verify userpassword and the URL path

--- Server/routers/auth.py
+++ Server/routers/auth.py
@@ -1,16 +1,24 @@
-import Schemas
 from fastapi import Depends, APIRouter, Body, HTTPException, status, Path
 from sqlalchemy.orm import Session
 
-from . import Schemas, models
+from .. import Schemas, models, utils
 from ..database import get_db
 
 router = APIRouter(tags=["Authentication"])
 
-@router.post("login")
+@router.post("/login")
 def login (user_details: Schemas.UserLogin, db: Session = Depends(get_db)):
 
   user = (db.query(models.Login).filter(models.Login.email == user_details.email).first())
 
   if not user:
     raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail=f"Invalid Credentials")
+  
+  if not utils.verify_password(user_details.password, user.password):
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN, detail=f"Invalid Credentials"
+        )
+
+  # access_token = Oauth.AccessToken(data={"username": user.username})
+  # return {"access_token": access_token, "token_type": "bearer"}
+  return{"Message":"Successfully logged In"}
