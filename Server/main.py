a323765335856441d556936ccf2a57bf2367cb37
AliuQudus <qudusaliu98@gmail.com>
2025-10-23 10:36:27 +0100

    Task: Added te router for google books

--- Server/main.py
+++ Server/main.py
@@ -1,7 +1,8 @@
 from fastapi import FastAPI
+#from routers import google_books
 from . import models
 from .database import engine
-from .routers import user, auth
+from .routers import user, auth, google_books, book
 
 models.Base.metadata.create_all(bind=engine)
 
@@ -9,6 +10,9 @@ app = FastAPI()
 
 app.include_router(user.router)
 app.include_router(auth.router)
+app.include_router(book.router)
+app.include_router(google_books.router)
+
 
 
 @app.get("/")
